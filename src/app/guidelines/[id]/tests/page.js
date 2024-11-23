51eda4fd6bb8d2ec2cbc58de047b85f89dae23e3
Hernan Macaya <hjmacaya@uc.cl>
2024-11-23 10:39:08 -0300

    fix: tests routing

---
+++ src/app/guidelines/[id]/tests/page.js
@@ -0,0 +1,64 @@
+'use client';
+import { UploadPage } from '@/components/UploadPage';
+import { Table } from '@/components/tables';
+import { configFile } from '../../../../config';
+
+export default function GuidelinesPage() {
+
+  // Table actions
+  const handleView = (test) => {
+    console.log("Viewing test:", test);
+  }
+  const handleEdit = (test) => {
+    console.log("Editing test:", test);
+  }
+  const handleDownload = (test) => {
+    console.log("Downloading test:", test);
+  }
+  const handleDelete = (test) => {
+    console.log("Deleting test:", test);
+  }
+
+  const testsMock = [
+    {
+      id: 1,
+      nombre: "Prueba de Pepito Perez",
+      puntajeTotal: "80/100",
+      desempeño: "80%",
+      link: "/tests/1"
+    },
+    {
+      id: 2,
+      nombre: "Prueba de Maria Gomez",
+      puntajeTotal: "80/100",
+      desempeño: "80%",
+      link: "/tests/2"
+    },
+    {
+      id: 3,
+      nombre: "Prueba de Juan Pérez",
+      puntajeTotal: "80/100",
+      desempeño: "80%",
+      link: "/tests/3"
+    }
+  ]
+  return (
+    <div>
+      {/* Drag and Drop Upload */}
+      <UploadPage apiEndpoint={`${configFile.API_BASE_URL}/prueba/`} method="POST" elementToDrop="pruebas" isMultipleFiles={true} />
+
+      {/* Tests Table */}
+      <div className="flex justify-center items-center my-4">
+        <h1 className="text-2xl font-bold">Tus Pruebas</h1>
+      </div>
+      <Table 
+        data={testsMock} 
+        styleVariant="style2" 
+        onView={handleView}
+        onEdit={handleEdit}
+        onDownload={handleDownload}
+        onDelete={handleDelete}
+      />
+    </div>
+  )
+}
--- src/config/config.js
+++ src/config/config.js
@@ -2,8 +2,6 @@ const configFile = {
   API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL
 }
 
-console.log("Config file:", configFile);
-
 export {
   configFile
 }
