0a195a306c6860f2004a61c4a50774e7f1947418
Hernan Macaya <hjmacaya@uc.cl>
2024-11-23 02:52:32 -0300

    add: BaseCard component

--- src/app/uiResources/page.js
+++ src/app/uiResources/page.js
@@ -1,16 +1,22 @@
 import { PrimaryButton, SecondaryButton, ThirdButton, ForthButton } from "@/components/buttons";
+import { BaseCard } from "@/components/cards";
 
 export default function UiResources() {
   return (
     <div className="min-h-full">
       <h1 className="text-4xl font-bold text-center">UI Resources</h1>
 
-      <div className="flex flex-col items-center justify-center">
-        <h2> UI Resources </h2>
+      <h2 className="text-2xl font-bold text-center"> Buttons </h2>
+      <div className="flex flex-col items-center justify-center gap-2 my-5">
         <PrimaryButton text="Primary Button" />
         <SecondaryButton text="Secondary Button" />
         <ThirdButton text="Third Button" />
         <ForthButton text="Forth Button" />
+      </div>
+
+      <h2 className="text-2xl font-bold text-center"> Cards </h2>
+      <div className="flex flex-row items-center justify-center gap-2 my-5">
+        <BaseCard title="Pauta Base" text="Esta es una pauta base para una card" image="/logo.jpeg" link="https://www.google.com" isClickable={true} hasImage={true} />
       </div>
     </div>
   )
---
+++ src/components/cards/BaseCard.js
@@ -0,0 +1,35 @@
+const BaseCard = ({ title, text, image, link, isClickable, hasImage }) => {
+  const Wrapper = isClickable ? 'a' : 'div';
+  const cardClasses = `max-w-sm rounded overflow-hidden shadow-lg ${
+    isClickable ? 'hover:shadow-xl transform hover:scale-105 transition duration-300' : ''
+  }`;
+
+  return (
+    <Wrapper
+      href={isClickable ? link : undefined}
+      className={cardClasses}
+    >
+      {hasImage && image && (
+        <img className="w-full" src={image} alt="Card Image" />
+      )}
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{title}</div>
+        <p className="text-gray-700 text-base">
+          {text}
+        </p>
+      </div>
+      {!isClickable && (
+        <div className="px-6 py-4">
+          <a
+            href={link}
+            className="bg-primary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Learn More
+          </a>
+        </div>
+      )}
+    </Wrapper>
+  );
+};
+
+export { BaseCard };
