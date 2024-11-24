35094185e569e8aaa4e4b0a530b25470a36fa21f
Hernan Macaya <hjmacaya@uc.cl>
2024-11-24 01:37:04 -0300

    fix: config file

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 let configFile = {};
 
 if (process.env.NEXT_PUBLIC_NODE_ENV === 'local') {
-  configFile.API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL_LOCAL;
+  configFile.API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL_LOCAL || '';
 } else {
   configFile.API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 }
