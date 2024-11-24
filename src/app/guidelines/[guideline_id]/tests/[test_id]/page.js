9a3d3a8449af9cd5e5a14d09943691e4016c130b
matias cox <mcox5@uc.cl>
2024-11-24 04:15:25 -0300

    feat(endpoints): fix result page

--- src/app/guidelines/[guideline_id]/tests/[test_id]/page.js
+++ src/app/guidelines/[guideline_id]/tests/[test_id]/page.js
@@ -1,50 +1,69 @@
+"use client"
 import { QuestionCard } from "@/components/cards";
+import { useState, useEffect } from 'react';
+import { useParams } from 'next/navigation';
+
+import { getAllQuestionsByGuidelineId, getAllStudentsAnswerByTestId } from '../../../../../api';
 
 export default function QuestionPage() {
-  const questionMockData = [
-    {
-      id: 1,
-      questionNumber: 1,
-      question: "¿Cuál es tu comida favorita?",
-      questionType: "development",
-      guidelineAnswer: "Pizza",
-      studentAnswer: "Pizza",
-      score: 10,
-      alternatives: null,
-      studentJustification: null,
-      guidelineJustification: null,
-    },
-    {
-      id: 2,
-      questionNumber: 2,
-      question: "¿Cuál es MI comida favorita?",
-      questionType: "multipleChoice",
-      guidelineAnswer: "B) Hamburguesa",
-      studentAnswer: "A) Pizza",
-      score: 0,
-      alternatives: {"A": "Pizza", "B": "Hamburguesa", "C": "Tacos", "D": "Sushi"},
-      studentJustification: null,
-      guidelineJustification: null,
-    },
-    {
-      id: 3,
-      questionNumber: 3,
-      question: "¿Son los tacos mi comida favorita?",
-      questionType: "trueOrFalse",
-      guidelineAnswer: "False",
-      studentAnswer: "False",
-      score: 10,
-      alternatives: null,
-      studentJustification: "No, pq le gusta la pizza",
-      guidelineJustification: "No, pq le gusta la hamburguesa",
-    }
-  ]
-  return (
-    <div className="flex flex-col gap-4 p-4">
-      {questionMockData.map((question) => (
-        <QuestionCard key={question.id} {...question} />
-      ))}
-    </div>
 
+  const params = useParams();
+  const testId = params['test_id'];
+  const guidelineId = params['guideline_id'];
+
+  const [questions, setQuestions] = useState([]);
+  const [studentAnswers, setStudentAnswers] = useState([]);
+
+  const fetchAllQuestions = async (guidelineId) => {
+    try {
+      const response = await getAllQuestionsByGuidelineId(guidelineId);
+
+      setQuestions(response.data);
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching questions:", error);
+      return null;
+    }
+  }
+
+  const fetchAllStudentAnswers = async (testId) => {
+    try {
+      const response = await getAllStudentsAnswerByTestId(testId);
+      console.log('responseData:', response.data);
+      setStudentAnswers(response.data);
+      return studentAnswers;
+    } catch (error) {
+      console.error("Error fetching student answers:", error);
+      return null;
+    }
+  }
+
+  const getStudentAnswerByQuestionId = (questionId) => {
+    console.log('questionId:', questionId);
+
+    return studentAnswers.find((studentAnswer) => studentAnswer.question_id === questionId);
+  }
+
+  useEffect(() => {
+    fetchAllQuestions(guidelineId);
+    fetchAllStudentAnswers(testId);
+  }, []);
+
+  return (
+      <div className="flex flex-col gap-4 p-4">
+        {questions?.map((question) => (
+          <QuestionCard
+            key={question.id}
+            questionNumber={question.positional_index}
+            questionType={question.type}
+            question={question.title}
+            guidelineAnswer={question.guideline_answer}
+            studentAnswer={getStudentAnswerByQuestionId(question.id)?.content}
+            studentScore={getStudentAnswerByQuestionId(question.id)?.student_score}
+            modelFeedback={getStudentAnswerByQuestionId(question.id)?.model_feedback}
+          />
+        ))}
+      </div>
+    // )
   )
 }
